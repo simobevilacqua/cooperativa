060365f532e7ca94ef34673f39b9bc44f05c0e1c
DuccioSonta <sontacchi.davide@gmailcom>
2020-05-16 19:34:59 +0200

    Aggiornamento database - tabella programma

--- assets/federazione.sql
+++ assets/federazione.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.7
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mag 16, 2020 alle 16:42
--- Versione del server: 10.1.30-MariaDB
--- Versione PHP: 7.2.2
+-- Creato il: Mag 16, 2020 alle 19:34
+-- Versione del server: 10.4.11-MariaDB
+-- Versione PHP: 7.4.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -30,10 +29,12 @@ USE `federazione`;
 -- Struttura della tabella `aggiorna`
 --
 
-CREATE TABLE `aggiorna` (
+CREATE TABLE IF NOT EXISTS `aggiorna` (
   `IDutente` int(11) NOT NULL,
   `IDprogramma` int(11) NOT NULL,
-  `data` date NOT NULL
+  `data` date NOT NULL,
+  PRIMARY KEY (`IDutente`,`IDprogramma`),
+  KEY `IDprogramma` (`IDprogramma`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -42,9 +43,11 @@ CREATE TABLE `aggiorna` (
 -- Struttura della tabella `autorizzato`
 --
 
-CREATE TABLE `autorizzato` (
+CREATE TABLE IF NOT EXISTS `autorizzato` (
   `IDutente` int(11) NOT NULL,
-  `IDprogramma` int(11) NOT NULL
+  `IDprogramma` int(11) NOT NULL,
+  PRIMARY KEY (`IDutente`,`IDprogramma`),
+  KEY `IDprogramma` (`IDprogramma`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -53,13 +56,15 @@ CREATE TABLE `autorizzato` (
 -- Struttura della tabella `pianificazione`
 --
 
-CREATE TABLE `pianificazione` (
-  `IDpianificazione` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `pianificazione` (
+  `IDpianificazione` int(11) NOT NULL AUTO_INCREMENT,
   `tipo` enum('giornaliero','settimanale','mensile') DEFAULT NULL,
   `giorno` varchar(40) DEFAULT NULL,
   `ora` time DEFAULT NULL,
-  `IDprogramma` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `IDprogramma` int(11) NOT NULL,
+  PRIMARY KEY (`IDpianificazione`),
+  KEY `IDprogramma` (`IDprogramma`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dump dei dati per la tabella `pianificazione`
@@ -74,14 +79,17 @@ INSERT INTO `pianificazione` (`IDpianificazione`, `tipo`, `giorno`, `ora`, `IDpr
 -- Struttura della tabella `processo`
 --
 
-CREATE TABLE `processo` (
-  `IDprocesso` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `processo` (
+  `IDprocesso` int(11) NOT NULL AUTO_INCREMENT,
   `inizio` date NOT NULL,
   `fine` date DEFAULT NULL,
   `stato` varchar(40) NOT NULL,
   `esito` varchar(40) DEFAULT NULL,
   `IDutente` int(11) DEFAULT NULL,
-  `IDprogramma` int(11) NOT NULL
+  `IDprogramma` int(11) NOT NULL,
+  PRIMARY KEY (`IDprocesso`),
+  KEY `IDutente` (`IDutente`),
+  KEY `IDprogramma` (`IDprogramma`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -90,12 +98,14 @@ CREATE TABLE `processo` (
 -- Struttura della tabella `programma`
 --
 
-CREATE TABLE `programma` (
-  `IDprogramma` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `programma` (
+  `IDprogramma` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(40) NOT NULL,
   `descrizioneLunga` varchar(400) DEFAULT NULL,
-  `IDprerequisito` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `IDprerequisito` int(11) DEFAULT NULL,
+  PRIMARY KEY (`IDprogramma`),
+  KEY `IDprerequisito` (`IDprerequisito`)
+) ENGINE=InnoDB AUTO_INCREMENT=133 DEFAULT CHARSET=latin1;
 
 --
 -- Dump dei dati per la tabella `programma`
@@ -103,7 +113,15 @@ CREATE TABLE `programma` (
 
 INSERT INTO `programma` (`IDprogramma`, `nome`, `descrizioneLunga`, `IDprerequisito`) VALUES
 (123, 'programma1', 'Descrizione lunga del programma1', NULL),
-(124, 'programma2', 'Descrizione lunga', 123);
+(124, 'programma2', 'Descrizione lunga', 123),
+(125, 'programma3', 'Descrizione programma3', NULL),
+(126, 'programma4', 'Descrizione programma4', 123),
+(127, 'programma5', 'Descrizione programma5', 124),
+(128, 'programma6', 'Descrizione programma6', 126),
+(129, 'programma7', 'Descrizione programma7', 127),
+(130, 'programma8', 'Descrizione programma8', NULL),
+(131, 'programma9', 'Descrizione programma9', 130),
+(132, 'programma10', 'Descrizione diversa', NULL);
 
 -- --------------------------------------------------------
 
@@ -111,9 +129,11 @@ INSERT INTO `programma` (`IDprogramma`, `nome`, `descrizioneLunga`, `IDprerequis
 -- Struttura della tabella `ricevenotifiche`
 --
 
-CREATE TABLE `ricevenotifiche` (
+CREATE TABLE IF NOT EXISTS `ricevenotifiche` (
   `IDutente` int(11) NOT NULL,
-  `IDprogramma` int(11) NOT NULL
+  `IDprogramma` int(11) NOT NULL,
+  PRIMARY KEY (`IDutente`,`IDprogramma`),
+  KEY `IDprogramma` (`IDprogramma`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -122,13 +142,14 @@ CREATE TABLE `ricevenotifiche` (
 -- Struttura della tabella `utente`
 --
 
-CREATE TABLE `utente` (
-  `IDutente` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `utente` (
+  `IDutente` int(11) NOT NULL AUTO_INCREMENT,
   `psw` varchar(40) NOT NULL,
   `nome` varchar(40) NOT NULL,
   `email` varchar(60) NOT NULL,
-  `tipo` enum('utente','admin') DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `tipo` enum('utente','admin') DEFAULT NULL,
+  PRIMARY KEY (`IDutente`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Dump dei dati per la tabella `utente`
@@ -137,87 +158,6 @@ CREATE TABLE `utente` (
 INSERT INTO `utente` (`IDutente`, `psw`, `nome`, `email`, `tipo`) VALUES
 (1, 'admin', 'admin', 'admin@gmail.com', 'admin'),
 (2, 'user', 'user', 'user@gmail.com', 'utente');
-
---
--- Indici per le tabelle scaricate
---
-
---
--- Indici per le tabelle `aggiorna`
---
-ALTER TABLE `aggiorna`
-  ADD PRIMARY KEY (`IDutente`,`IDprogramma`),
-  ADD KEY `IDprogramma` (`IDprogramma`);
-
---
--- Indici per le tabelle `autorizzato`
---
-ALTER TABLE `autorizzato`
-  ADD PRIMARY KEY (`IDutente`,`IDprogramma`),
-  ADD KEY `IDprogramma` (`IDprogramma`);
-
---
--- Indici per le tabelle `pianificazione`
---
-ALTER TABLE `pianificazione`
-  ADD PRIMARY KEY (`IDpianificazione`),
-  ADD KEY `IDprogramma` (`IDprogramma`);
-
---
--- Indici per le tabelle `processo`
---
-ALTER TABLE `processo`
-  ADD PRIMARY KEY (`IDprocesso`),
-  ADD KEY `IDutente` (`IDutente`),
-  ADD KEY `IDprogramma` (`IDprogramma`);
-
---
--- Indici per le tabelle `programma`
---
-ALTER TABLE `programma`
-  ADD PRIMARY KEY (`IDprogramma`),
-  ADD KEY `IDprerequisito` (`IDprerequisito`);
-
---
--- Indici per le tabelle `ricevenotifiche`
---
-ALTER TABLE `ricevenotifiche`
-  ADD PRIMARY KEY (`IDutente`,`IDprogramma`),
-  ADD KEY `IDprogramma` (`IDprogramma`);
-
---
--- Indici per le tabelle `utente`
---
-ALTER TABLE `utente`
-  ADD PRIMARY KEY (`IDutente`);
-
---
--- AUTO_INCREMENT per le tabelle scaricate
---
-
---
--- AUTO_INCREMENT per la tabella `pianificazione`
---
-ALTER TABLE `pianificazione`
-  MODIFY `IDpianificazione` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT per la tabella `processo`
---
-ALTER TABLE `processo`
-  MODIFY `IDprocesso` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT per la tabella `programma`
---
-ALTER TABLE `programma`
-  MODIFY `IDprogramma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=125;
-
---
--- AUTO_INCREMENT per la tabella `utente`
---
-ALTER TABLE `utente`
-  MODIFY `IDutente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Limiti per le tabelle scaricate
